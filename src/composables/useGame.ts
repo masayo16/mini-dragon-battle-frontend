274639687a01d935a3d0e5c5d957a02c584ff939
masayo16 <masayo16d@gmail.com>
2025-06-25 23:46:58 +0900

    upd: 古いプログラムの削除

--- src/composables/useGame.ts
+++ src/composables/useGame.ts
@@ -1,6 +1,7 @@
 import * as PIXI from 'pixi.js';
 
 import type { Ref } from 'vue';
+
 import { Player } from '~/game/entities/Player';
 import { gridToPixel, type GridPos } from '~/game/grid/Grid';
 import { loadLevel } from '~/game/grid/LevelLoader';
@@ -17,36 +18,12 @@ export async function useGame(container: Ref<HTMLElement | null>) {
   });
   container.value.appendChild(app.canvas);
 
-  await loadLevel('assets/level/level1.txt', app.stage);
+  await loadLevel('/assets/level/level1.txt', app.stage);
 
   const player = new Player();
   await player.init();
   player.position.set(...Object.values(gridToPixel({ col: 14, row: 23 })));
   app.stage.addChild(player);
-
-  // const keys = new Set<string>();
-  // const onKey = (e: KeyboardEvent) =>
-  //   e.type === 'keydown' ? keys.add(e.key) : keys.delete(e.key);
-
-  // window.addEventListener('keydown', onKey);
-  // window.addEventListener('keyup', onKey);
-
-  // const speed = 5;
-  // app.ticker.add(ticker => {
-  //   const delta = ticker.deltaTime;
-
-  //   if (keys.has('ArrowLeft')) {
-  //     player.x -= speed * delta;
-  //   }
-  //   if (keys.has('ArrowRight')) {
-  //     player.x += speed * delta;
-  //   }
-
-  //   player.x = Math.max(
-  //     player.width / 2,
-  //     Math.min(player.x, app.screen.width - player.width / 2),
-  //   );
-  // });
 
   const onArrowKey = (e: KeyboardEvent) => {
     const map: Record<string, GridPos> = {

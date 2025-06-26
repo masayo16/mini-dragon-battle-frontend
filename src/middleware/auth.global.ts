a9b7fba19c7e7fbe2f8c8f764eb48a04f3dd4249
masayo16 <masayo16d@gmail.com>
2025-06-26 23:46:50 +0900

    upd: 不要な出力

--- src/middleware/auth.global.ts
+++ src/middleware/auth.global.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(to => {
   const user = useGoogleAuth().user;
-  console.log('auth.global.ts', user.value, to.path);
+
   if (!user.value && to.path !== '/login' && to.path !== '/') {
     return navigateTo('/login');
   }

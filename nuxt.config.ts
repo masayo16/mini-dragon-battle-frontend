6ea513e7022e04c0459a0c5111943420de0044a9
masayo16 <masayo16d@gmail.com>
2025-07-18 19:32:31 +0900

    upd: ssr使ってみるのはまた今度

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: 'src',
-  ssr: true,
+  ssr: false,
   nitro: {
     preset: 'static',
     prerender: {
